3d94b3da8d664ac8bd711a79a0dac02a50077ea6
GDevGroup <gpirasking@outlook.it>
2018-01-13 14:45:56 +0100

    Revert : Zero-common : Reenable GPU to CPU transition This reverts commit 2a66808

--- zero-common.mk
+++ zero-common.mk
@@ -195,6 +195,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.force_voice_config=wide \
     ro.nfc.sec_hal=true \
     wifi.interface=wlan0 \
+    ro.bq.gpu_to_cpu_unsupported=1 \
     debug.hwc.force_gpu=1 \
     video.accelerate.hw=1 
 
