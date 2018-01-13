8bd91d96751a5a6b241aca0e6188f4522f3ebafe
GDevGroup <gpirasking@outlook.it>
2018-01-13 22:25:06 +0100

    CameraHal1 : Add Instagram Signed-off-by : Giugiu19 <gpirasking@outlook.it>

--- zero-common.mk
+++ zero-common.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += \
     
 # build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
-       camera.hal1.packagelist=com.skype.raider,com.whatsapp
+       camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.instagram.android
 
 # DTB    
 PRODUCT_PACKAGES += \
